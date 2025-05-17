a26db77fa4bf4d27fc8b2ce5c4aa6272b8abc352
usamoi <usamoi@outlook.com>
2025-05-17 11:57:07 +0800

    IZe981Pb6cgAkhNjB2Z9jBk6zJCyVzXVDqCjduhW

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,1 @@
-unsafe extern "C" {
-    unsafe static UNDEFINED: usize;
-}
-
-#[unsafe(no_mangle)]
-pub fn used() {
-    println!("UNDEFINED = {}", unsafe { UNDEFINED });
-}
-
-pub fn marker() -> usize {
-    0
-}
-
-fn main() {
-    assert_eq!(marker(), 0_usize);
-}
+fn main() {}
